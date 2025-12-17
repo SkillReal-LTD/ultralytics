f562a09ece0bbf6b8afaa55ec91c819b7789f106
LeDuySon <leduyson2632001@gmail.com>
2025-12-17 08:16:35 +0000

    chore

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,14 +1,19 @@
-from ultralytics import YOLO
+from ultralytics import YOLO, settings
+
+settings.update({"wandb": True})
 
 # Load model
 model = YOLO('yolo11n.pt')
 
 results = model.train(
+    project="ultralytics",
+    name="test_artifacts",
     task='detect',
     data='/home/ubuntu/ultralytics/configs/test_skillreal.yaml',
-    epochs=5,
+    epochs=4,
     imgsz=640,
     batch=16,
+    save_period=1,  # Save checkpoint every epoch (1, 2, 3, 4, 5)
     fitness_weight=[0.0, 0.9, 0.1, 0.0],
 )
-print(model.names)
+print(model.trainer.best_epoch)
